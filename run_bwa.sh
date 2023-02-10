bedee5f8a830f0effe29a6eb0306e74f8e5bf379
Peep Kolberg <peepkolberg@gmail.com>
2023-02-10 10:28:18 +0200

    Load samtools module

--- run_bwa.sh
+++ run_bwa.sh
@@ -10,6 +10,7 @@
 # TODO: Tweak bwa_mem memory parameter. 8 GB was too little, 32 GB worked, and I couldn't be bothered with testing further at the time.
 
 module load bwa/0.7.17
+module load any/samtools
 module load nextflow
 
 ref_gen_prefix=Homo_sapiens.GRCh38.dna.primary_assembly  # Change this to your reference genome's filename but strip '.fa' from the end
@@ -18,4 +19,4 @@ nextflow -log logs run bwa.nf -profile tartu_hpc \
     --samples data/samples.tsv \
     --ref_gen bwa_idx \
     --ref_gen_prefix $ref_gen_prefix \
-#    -resume
+    -resume
